29b9a4193d5ed2cc2e8a4759b15ec06f8ee8d3e2
Ilia <istudyatuni@gmail.com>
2025-01-30 19:55:32 +0300

    implement

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result};
+use aspasia::{AssSubtitle, Subtitle};
+use clap::{Parser, ValueHint};
+
+fn main() -> Result<()> {
+    let Cli {
+        path,
+        font,
+        no_backup,
+    } = Cli::parse();
+
+    let paths = if path.is_dir() {
+        glob::glob("./*.ass")
+            .context("failed to find .ass files in current directory")?
+            .map(|res| res.map_err(Into::into))
+            .collect::<Result<Vec<_>>>()?
+    } else {
+        vec![path]
+    };
+
+    for p in paths {
+        if !no_backup {
+            let bak = p.with_extension("ass.bak");
+            if !bak.exists() {
+                std::fs::copy(&p, bak).context("failed to backup file")?;
+            }
+        }
+        fix_file(&p, &font)?;
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// Path to .ass file or directory with .ass files
+    #[clap(value_hint = ValueHint::AnyPath)]
+    path: PathBuf,
+    /// Font name to set
+    font: String,
+    /// Do not create backup files
+    #[clap(long)]
+    no_backup: bool,
+}
+
+fn fix_file(path: &Path, font: &str) -> Result<()> {
+    let mut ass = AssSubtitle::from_path(path)
+        .with_context(|| format!("failed to read {}", path.display()))?;
+
+    ass.styles_mut()
+        .iter_mut()
+        .for_each(|s| s.fontname = font.to_string());
+
+    ass.export(path)?;
+
+    Ok(())
+}
